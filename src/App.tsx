386f880a0e01564831244ba7eda37ee4efb97de8
varesilva <varesilva@rd.com.br>
2024-10-23 19:22:34 -0300

    creating account module

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Welcome from './modules/account/mobile/src/screens/welcome/welcome';
+
+function App(): React.JSX.Element {
+  return <Welcome></Welcome>;
+}
+
+export default App;
---
+++ src/modules/account/mobile/src/screens/welcome/welcome.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Welcome = (): JSX.Element => {
+  return <Text>WELCOME</Text>;
+};
+
+export default Welcome;
